47cb1dfa3fa0c907e8ad794540d997e7a4c5ff85
adarshjithu <118181229+adarshjithu@users.noreply.github.com>
2023-10-19 23:38:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+///function createpost
+document.querySelector(".create-post").classList.toggle("hide");
+
+         const create=()=> {
+
+     document.querySelector(".create-post").classList.toggle("hide");
+}
+
+
+document.querySelector(".searchBar").classList.toggle("hide");
+         const search=()=> {
+      
+          document.querySelector(".searchBar").classList.toggle("hide");
+}
+
+
+
+;
